5da887a63d44a13ae26cfff92eebe13d931fae3a
WANG Zhiyu <wangzhiyu0471@gmail.com>
2020-08-11 09:34:15 +0800

    CMake add 1_simple project

---
+++ CMake/1_simple/func.h
@@ -0,0 +1,6 @@
+#ifndef __FUNC_H
+#define __FUNC_H
+
+int func();
+
+#endif
---
+++ CMake/1_simple/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "func.h"
+
+int main(){
+    std::cout << func() << std::endl;
+    return 0;
+}
